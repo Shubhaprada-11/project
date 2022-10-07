fe1b9b990858aaad9f75bed56e152c9d17a1f272
Samhitha S <samhitha1913@gmail.com>
2022-10-07 12:56:54 +0530

    second

--- src/Details.js
+++ src/Details.js
@@ -1,57 +1,100 @@
-const axios = require('axios').default
+import React from "react";
+import { useState, useEffect } from "react";
 const API = "http://localhost:8000/api";
-
-
+const assignmentId = "633efb26c56cf8de7bf06aa6";
+const userId = "633e3946e6834326fd628855";
+const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzNlMzk0NmU2ODM0MzI2ZmQ2Mjg4NTUiLCJpYXQiOjE2NjUwNzE4MzZ9.DhKPBvMw-yWzClfjfNmpUTfSY002dPiIiEearSa0plk";
 // const userId = isAutheticated() && isAutheticated().user._id;
 // const token = isAutheticated() && isAutheticated().token;
-const getData = axios.get('${API}/assignment/${assignmentId}/${userId}').then(response => response.data);
-const checkboxes=[];
-checkboxes=getData.checkboxes;
-const userData=axios.get('${API}/user/${userId}').then(response =>response.data);
-const subdiv=[];
-subdiv=userData.subdivisions;
-function Details(){
-return(
-      <div>
-      {getData.map((data)=>(<li key={data._id}>
+
+const url1 = `${API}/assignment/${assignmentId}/${userId}`;
+const url2 = `${API}/user/${userId}`;
+let  checkboxes=[], subdiv=[];
+// const newArray=[];
+// newArray.len=checkboxes.len ;  
+// for(let j =0; j<newArray.len;j++){
+//       newArray[j]="<input type= 'checkbox' checked={checkboxes[0]}> </input>";
+// }
+
+const Details = () => {
+      const [getData, setData] = useState([]);
+      const fetchingData = async () => {
+            const config ={
+                        method: 'GET',
+                        headers: {
+                              'Authorization': 'Bearer ' + token,
+                        }
+            }
+            try {
+              const res1 = await fetch(`${url1}`);
+              if (!res1.ok) {
+                throw new Error("Something went wrong");
+              }
+              const newGetData = await res1.json();
+
+              //getData = newGetData;
+              console.log(newGetData);
+              const newcheckboxes = newGetData.checkboxes;
+              checkboxes = newcheckboxes;
+              console.log(checkboxes);
+              
+              /*const newSubdiv = newGetData.subdivisions;
+              subdiv = newSubdiv;
+              console.log(subdiv);*/
+              setData(newGetData);
+
+              
+             /* const res2 = await fetch(`${url2}`);
+              if (!res2.ok) {
+                throw new Error("Something went wrong");
+              }
+              const userData = await res2.json();
+          
+              const newSubdiv = userData.subdivisions;
+              subdiv = newSubdiv;
+              console.log(subdiv);*/
+            }
+            catch (err) {
+              console.log("inside error");
+              console.log(err.message);
+            }
+      };
+
+      useEffect(() => {
+            fetchingData();
+          }, [])
+      
+          let text = "";
+          for (let i = 0; i < checkboxes.length; i++) {
+            text += "<input type= 'checkbox' checked={checkboxes[0]}>hello </input><br />";
+          } 
+          
+      //getData={};
+      return (
             <div>
-                  {data.status}
+              <li key={getData._id}>
+                <div>{getData._id}</div>
+                <div>{getData.status}</div>
+                <p id="demo"></p>
+                <script type="text/javascript">func();
+                function func(){
+                  document.getElementById("demo").innerHTML = text;
+                };
+                </script>
+                <div>
+
+                
+
+                </div>
+              </li>
+              
             </div>
-      </li>))}
-      </div>
-)
-            {/* <p>Assignment Id</p>
-      <pre>
-            <code>
-                  {JSON.stringify(getData._id,null,2)}
-                  </code>
-            </pre>
-            <p>Assignment Id</p>
-      <pre>
-            <code>
-                  {JSON.stringify(getData.status,null,2)}
-                  </code>
-            </pre>
-            <p>Assignment Id</p>
-      <pre>
-            <code>
-                  {JSON.stringify(getData.checkboxes,null,2)}
-                  </code>
-            </pre>
-      </div> */}
+
+          );
+          
+        };
+
       
-            
-         /* <p>Job ID ${getData.job}</p> 
-         <p>Assignemnt ID ${getData._id}</p> 
-         <p>Status ${getData.status}</p>
-         
-         <input type="checkboxes"></input>
-         
-         <p>${getData.comments}</p> 
-         axios.post("/assignment/:assignmentId/comment/:userId");  */
      
-      
-
-}
-
+   
 export default Details;
