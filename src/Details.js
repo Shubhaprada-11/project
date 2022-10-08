08ecd08542e4a450831ed8e174db178fae38c7a2
Samhitha S <samhitha1913@gmail.com>
2022-10-08 11:24:54 +0530

    comments

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,16 @@
 import React from "react";
+import axios from 'axios';
 import { useState, useEffect } from "react";
 const API = "http://localhost:8000/api";
 const assignmentId = "633efb26c56cf8de7bf06aa6";
+const assignmentId2 = "6340d42ec1fe7c53798d397d";
 const userId = "633e3946e6834326fd628855";
 const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzNlMzk0NmU2ODM0MzI2ZmQ2Mjg4NTUiLCJpYXQiOjE2NjUwNzE4MzZ9.DhKPBvMw-yWzClfjfNmpUTfSY002dPiIiEearSa0plk";
 // const userId = isAutheticated() && isAutheticated().user._id;
 // const token = isAutheticated() && isAutheticated().token;
-
 const url1 = `${API}/assignment/${assignmentId}/${userId}`;
 const url2 = `${API}/user/${userId}`;
+const urlc = `${API}/assignment/${assignmentId2}/comment/${userId}`;
 let  checkboxes=[], subdiv=[];
 // const newArray=[];
 // newArray.len=checkboxes.len ;  
@@ -19,6 +21,7 @@ let  checkboxes=[], subdiv=[];
 const Details = () => {
       const [getData, setData] = useState([]);
       const [checked, setChecked] = useState([]);
+      const [value, setValue] = useState('');
       const fetchingData = async () => {
             const config ={
                         method: 'GET',
@@ -77,12 +80,32 @@ const Details = () => {
             setChecked(updatedList);
           };
           // Generate string of checked items
-          const checkedItems = checked.length
-            ? checked.reduce((total, item) => {
+          const checkedItems = checked.length ? checked.reduce((total, item) => {
                 return total + ", " + item;
               })
             : "";
-        
+            
+            const handleClick = event => {
+                  event.preventDefault();
+                  const CommentObject = {
+                        user: "633e3946e6834326fd628855",
+                        comment: this.state.value,
+                        time: Date().toLocaleString(),
+                  };
+                  axios.post(`${urlc}`, CommentObject)
+                  .then((res) => {
+                  console.log(res.data)
+                  }).catch((error) => {
+                  console.log(error)
+                  });
+                  setValue(event.target.value);
+                  console.log(event.target.value);
+            };
+
+            const handleValueChange = event => {
+                  setValue(event.target.value);
+                  console.log(event.target.value);
+            };
       //getData={};
       return (
             <div>
@@ -99,10 +122,21 @@ const Details = () => {
                   </div>
                   ))}            
                 </div>
-                {`Items checked are " ${checkedItems}`}
+                {`Subdivisions checked are " ${checkedItems}`}
+                <div>
+                <label htmlFor="message">Comments</label>
+                <br></br>
+                  <textarea
+                  id="message"
+                  name="message"
+                  value={value}
+                  onChange={handleValueChange}
+                  /><br></br>
+                  <button onClick={handleClick}>Submit</button>
+             </div>
             </div>
-
           );
+          
           
         };
 
